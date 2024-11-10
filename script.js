19d4ee7af47ad118ada15f892d28cf4d6a00399f
tris-max <acharya.t@northeastern.edu>
2024-11-10 18:18:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Function to show the welcome alert message
+function showWelcomeMessage() {
+    alert("Welcome pets of all ages, sizes and skills");
+}
+// Event listener for when the page is loaded
+window.onload = showWelcomeMessage;
